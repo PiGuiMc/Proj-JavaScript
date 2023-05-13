9cd9d96ba55533ff244e62705e90d704b0307f84
José Guilherme <98156891+PiGuiMc@users.noreply.github.com>
2023-05-13 11:45:50 -0300

    Commit
    Commit

---
+++ App-Prev-Tempo/js/scripts.js
@@ -0,0 +1,32 @@
+const key = "07f6dfc4188ffd0a7b2a3f566fd6254c"
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("input").addEventListener("keypress", function(event){
+        if(event.keyCode == 13){
+            cliqueiNoBotao();
+        }
+    });
+});
+
+async function buscarCidade(cidade){
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+    
+    colocarDadosNaTela(dados)
+}
+
+function cliqueiNoBotao(){
+    const cidade = document.querySelector(".input-cidade").value;
+    buscarCidade(cidade);
+}
+
+function colocarDadosNaTela(dados){
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = `Tempo em ${dados.name} - ${dados.sys.country}`;
+
+    document.querySelector(".temp").innerHTML = `${Math.floor(dados.main.temp)}°C`;
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+    document.querySelector(".texto-previsao").innerHTML = (dados.weather[0].description);
+
+    document.querySelector(".umidade").innerHTML = `Umidade ${dados.main.humidity}%`;
+}
---
+++ Readme.txt
@@ -0,0 +1,1 @@
+Repositório para projeto em apredizado sobre JavaScript
